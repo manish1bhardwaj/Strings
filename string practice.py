d67d5e4f306ab8ef356c6c7e27c3e1a8a87274bf
Manish bhardwaj <142594633+manish1bhardwaj@users.noreply.github.com>
2023-10-29 22:19:07 +0530

    complete strings practice

---
+++ string practice.py
@@ -0,0 +1,112 @@
+# string="15"
+# number=7
+# string_number=int(string)
+# out=string_number + number
+# print('sum is',out)
+
+
+# a=1
+# print(type(a))
+
+# a=3.0
+# print(a,type(a))
+
+
+# a=1
+# b=2.0
+# c=a+b
+# print(c,type(c))
+
+
+# a=  "he said, \"he want to eat an apple\""
+# print(a, type(a))
+
+
+# a="""Machine learning (ML) is a branch 
+# of artificial intelligence (AI)
+# that 
+# enables computers to “self-learn” from 
+# training data and improve 
+# over time, 
+# without being
+# explicitly programmed. Machine learning
+# algorithms are able to detect patterns 
+# in data and learn from them, 
+# in order to make their own predictions."""
+
+# # print(a,type (a))
+
+
+
+# # name = "manish"
+
+# print("now we use \nfor loop \nto continue")
+
+# for carracter in a:
+# print(carracter)
+
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# print(name[4])
+# print(name[5])
+
+
+#slicing of strings and finding length of strings :
+
+# names= "manish,shivam,abhishek"
+# names= "manish"
+# print(len(names))
+# print(names[-4:-3])
+# print(names[1:4])
+
+
+# a="""Machine learning (ML) is a branch 
+# of artificial intelligence (AI)
+# that 
+# enables computers to “self-learn” from 
+# training data and improve 
+# over time, 
+# without being
+# explicitly programmed. Machine learning
+# algorithms are able to detect patterns 
+# in data and learn from them, 
+# in order to make their own predictions."""
+
+# print(len(a),a[0:2])
+
+
+# names= "manish,shivam,abhishek @@@!!!!$$"
+# print(names.replace("manish","ram",))
+# print(type(names))
+# print(names.upper())
+# print(names.lower())
+# print(names.rstrip("@,!,$"))
+# print(names.split())
+
+# a="hello everyone i'm manish Bhardwaj"
+# b="hello everYone i'm maNisH BhArdwaj"
+# print(a.capitalize())
+# print(b.capitalize())
+
+# a = "welcome to my world to enjoy and fun"
+# print(a.center(50,"."))
+# print(a.count("o"))
+# print(a.endswith("to",2,10))
+# print(a.find("my"))
+# print(a.find("myyyy"))
+# print(a.index("my"))
+# print(a.index("myyyy"))
+# print(a.isalnum())
+# print(a.isalpha())
+# print(a.islower())
+# print(a.isprintable())
+# print(a.isspace())
+# print(a.istitle())
+# print(a.isupper())
+# print(a.startswith("Welcome"))
+# print(a.swapcase())
+# print(a.title())
+
+
